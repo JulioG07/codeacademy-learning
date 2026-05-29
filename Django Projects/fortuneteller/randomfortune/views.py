fb626aac18868690e02197888f90d71fba7cfd2b
JulioG07 <juliogoitia12@gmail.com>
2026-05-29 18:44:12 -0400

    added an additional view function and also had it display images

--- Django Projects/fortuneteller/randomfortune/views.py
+++ Django Projects/fortuneteller/randomfortune/views.py
@@ -9,10 +9,40 @@ fortuneList = [
   "Continue the grind, king!"
 ]
 
+meFacts = [
+  "You are Venezuelan",
+  "You are 19 years old",
+  "You go to Lehigh University",
+  "You fence and play volleyball for fun",
+  "You love Romeo Santos"
+]
+
+imageList = [
+  "/media/crush.jpg",
+  "/media/mexico.webp",
+  "/media/internship.jpg",
+  "/media/yapping.jpg",
+  "/media/king.webp",
+]
+
 # Create your views here.
 def fortune(request):
   fortune = random.choice(fortuneList)
+  fortune_index = fortuneList.index(fortune)  # index of the selected fortune
+
   context = {
-    "fortune" : fortune
+    "message" : "Here is your fortune",
+    "fullname" : "Julio Goitia",
+    "fortune" : fortune,
+    "img" : imageList[fortune_index]
+  }
+  return render(request, "randomfortune/fortune.html", context)
+
+def bio(request):
+  fact = random.choice(meFacts)
+  context = {
+    "message" : "Fun fact about ",
+    "fullname" : "Julio Goitia",
+    "fortune" : fact
   }
   return render(request, "randomfortune/fortune.html", context)
